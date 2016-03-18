4b8d16c42137b08620e3d1af08f27b81d2b74d50
Andrea Cremaschi <cremaschiandrea@gmail.com>
2016-03-18 15:01:36 +0000

    Further refactor and handle complex chains of superviews

--- CopyPaper/UIView+CopyPaper.swift
+++ CopyPaper/UIView+CopyPaper.swift
@@ -24,25 +24,28 @@ extension UIView {
     }
 
     public func cp_hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
-
         guard let hitView = self.cp_hitTest(point, withEvent: event) else { return nil }
+        return hitView.cp_passThroughTest(point, withEvent: event)
+    }
+    
+    private func cp_passThroughTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
 
-        // If the gesture shouldn't pass through, return the hit view
-        guard hitView.passThrough else { return hitView }
-
-        // If the gesture should pass through but there is no super view, return nil
+        // If the gesture shouldn't pass through, return self
+        guard self.passThrough else { return self }
+        
+        // If the gesture should pass through but there are no siblings views underneath
+        // search for the first underneath sibling view in the superviews hierarchy
         guard let underneathSiblingView = self.underneathSiblingView() else {
-
-            // If the gesture should pass through but there are no siblings views underneath
-            // hit test the underneath sibling view of the superview
-            guard let superview = self.superview else { return nil }
-            guard superview.passThrough else { return superview }
-            guard let superUnderneathSiblingView = superview.underneathSiblingView() else { return nil }
-            let pointInView = superUnderneathSiblingView.convertPoint(point, fromView: self)
-            return superUnderneathSiblingView.hitTest(pointInView, withEvent: event)
+            
+            guard let superview = self.superview else {
+                // No underneath sibling, no superview: someone set passThrough = true to the main UIWindow!
+                return nil
+            }
+            let pointInSuperview = superview.convertPoint(point, fromView: self)
+            return superview.cp_passThroughTest(pointInSuperview, withEvent: event)
         }
         
-        // Test the underneath sibling for hit
+        // Test the underneath sibling view for hit
         let pointInView = underneathSiblingView.convertPoint(point, fromView: self)
         return underneathSiblingView.hitTest(pointInView, withEvent:event)
     }
